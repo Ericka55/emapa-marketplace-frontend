0bc3a9f028d3c7393383d403b75900e8acb56858
Ericka55 <erickamorat55@gmail.com>
2026-06-02 12:41:40 -0400

    fix register form (rol/tipo mapping)

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,8 +11,7 @@ export default function Register() {
         email: "",
         password: "",
         telefono: "",
-        tipo: "COMPRADOR",
-
+rol: data.tipo,
         // negocio
         negocio: "",
         descripcionNegocio: "",
@@ -78,11 +77,10 @@ export default function Register() {
                 <input name="telefono" placeholder="Teléfono" onChange={handleChange} />
                 <input name="password" type="password" placeholder="Contraseña" onChange={handleChange} />
 
-                <select name="tipo" onChange={handleChange}>
-                    <option value="COMPRADOR">Soy comprador</option>
-                    <option value="VENDEDOR">Quiero ser vendedor</option>
-                </select>
-
+                <select name="rol" onChange={handleChange}>
+  <option value="COMPRADOR">Soy comprador</option>
+  <option value="VENDEDOR">Quiero ser vendedor</option>
+</select>
                 {form.tipo === "VENDEDOR" && (
                     <>
                         <input name="negocio" placeholder="Nombre negocio" onChange={handleChange} />
